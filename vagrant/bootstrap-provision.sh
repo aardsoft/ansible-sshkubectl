c544ea61a6ca73d3fda09ee8c9c517d4d91309bf
Bernd Wachter <bernd-github@wachter.fi>
2025-05-08 16:04:35 +0300

    Baseline commit

---
+++ vagrant/bootstrap-provision.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+JAILHOST=$1
+
+if ! which ansible-playbook 2>/dev/null; then
+  sudo apt-get -y update
+  sudo apt-get -y install python3-pip
+  pip install ansible==2.10.7
+  hash -r
+fi
+
+# ansible localhost -m yum -a 'name=wget,unzip,zip,python,libselinux-python,git state=present'
+chmod 0600 ~/vagrant_insecure_private_key
+
+# for some reason vbox always mounts /vagrant as root+0755, regardless of what we set on it in the vagrantfile
+chmod 0777 /vagrant /vagrant/vagrant
+
+cd /vagrant/vagrant
+
+ansible --version
+
+echo "Provisioning JAILHOST=$JAILHOST"
+
+ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook \
+  -i "$JAILHOST," \
+  --private-key ~/vagrant_insecure_private_key \
+  -e ansible_python_interpreter=/usr/local/bin/python2.7 \
+  configure-jailhost.yml
